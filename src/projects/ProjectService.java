44847958d10b5328e5a6ba124216e981e0c56542
AbdullahHumayunkabeer <566654@cognizant.com>
2026-09-06 22:00:39 +0545

    Add Project service

--- src/projects/ProjectService.java
+++ src/projects/ProjectService.java
@@ -6,4 +6,27 @@ import java.util.List;
 
 
 public class ProjectService {
+
+
+    public void create(Project project) {
+
+    }
+
+
+    public void updateStatus(Long id, String status) {
+
+    }
+
+
+    public List<Project> getByTeam(Long teamId) {
+
+        return new ArrayList<>();
+
+    }
+
+
+    public void delete(Long id) {
+
+    }
+
 }
